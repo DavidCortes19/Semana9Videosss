1848b8eecbc0c2e093c91707bc91b88cfc32a98c
DavidCortes19 <d.cortes72@alumnos.santotomas.cl>
2023-10-20 00:38:13 -0300

    Initial commit

---
+++ app/src/main/java/com/example/semana9videosss/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.semana9videosss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        VideoView videoView = findViewById(R.id.viVisor);
+        // Reemplaza "tu_ruta_del_video" con la ubicación del archivo de video en tu proyecto.
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.maluma_view;
+
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+
+        videoView.start();
+    }
+}
